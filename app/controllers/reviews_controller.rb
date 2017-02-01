eb7e1a92f615a943a63eb8aa08ed4fe417f56aff
Emmanuel Faboya <efaboya@hotmail.co.uk>
2017-02-01 00:27:58 +0000

    categories

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,15 +1,27 @@
 class ReviewsController < ApplicationController
+    before_action :find_review, only: [:show, :edit, :update, :destroy]
     
     def index
-       @reviews = Review.all.order("created_at DESC")
+    if params[:category].blank?
+			@reviews = Review.all.order("created_at DESC")
+	     else
+			@category_id = Category.find_by(name: params[:category]).id
+			@reviews = Review.where(:category_id => @category_id).order("created_at DESC")
+    end
+    end
+    
+    def show
+       @review = Review.find(params[:id])
     end
     
     def new
-        @review = Review.new
+        @review = current_user.reviews.build
+        @categories = Category.all.map{ |c| [c.name, c.id] }      
     end
     
     def create
-        @review = Review.new(review_params)
+        @review = current_user.reviews.build(review_params)
+        @review.category_id = params[:category_id]   
         
         if @review.save
 			redirect_to root_path
@@ -18,10 +30,32 @@ class ReviewsController < ApplicationController
 	end
     end
     
+    def edit
+        @categories = Category.all.map{ |c| [c.name, c.id] }
+    end
+    
+    def update
+        @review.category_id = params[:category_id] 
+        if @review.update(review_params)
+			redirect_to review_path(@review)
+		else
+			render 'edit'
+	end
+    end
+    
+    def destroy
+        @review.destroy
+        redirect_to root_path
+    end
+    
     private
      
       def review_params
-          params.require(:review).permit(:headline, :summary, :link, :writer)
+          params.require(:review).permit(:headline, :summary, :link, :writer, :category_id)
+      end
+      
+      def find_review
+         @review = Review.find(params[:id]) 
       end
       
 end
